df33fb2b29dc72014bfabca0887bb62d04b7c979
Alex Krusz <harfatum@gmail.com>
2015-02-21 22:11:54 -0500

    added defense behavior for enemies

--- enemy.js
+++ enemy.js
@@ -1,36 +1,34 @@
-function enemy(maxHp, attack, defense, atkBehavior,defBehavior,image) {
+function enemy(maxHp, attack, defense, atkBehavior, defBehavior, image) {
   this.maxHp = maxHp;
   this.attack = attack;
   this.defense = defense;
   this.atkBehavior = atkBehavior;
   this.defBehavior = defBehavior;
+  this.cssClass= ;
   this.currentHp = this.maxHp;
   this.turns = 0;
-  this.image="cssClass";
 }
 
-function atkBehavior(playerHp,orbData){
+function standardAtkBehavior(playerHp, orbData){
   // returns the total damage.
   // makes any necessary changes to orbs directly
-  //return this.attack;
 
   switch(this.turns % 5){
     case 0:
       return this.attack;
     case 1:
-      return this.attack * 2;
+      return this.attack;
     case 2:
-      return this.attack/2;
+      return this.attack;
     case 3:
-      return this.attack * 2;
+      return 0;
     case 4:
-      return this.attack/2;
-
+      return this.attack * 2;
   }
 
 }
 
-function defBehavior(matches){
+function standardDefBehavior(matches){
   var damage = scoreMatches(matches);
   this.currentHp -= damage;
 
