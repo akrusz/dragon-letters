d7fba8aa2d2ce732737b5cfcc04514a55068245b
Alex Krusz <harfatum@gmail.com>
2015-02-22 02:46:02 -0500

    broke out config api, wrote more

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-/*
- 	totalRows: total rows in board
- 	totalCols: total columns in board
- 	rowHeight: height of row in pixels
- 	colWidth: width of column in pixels
- 	minColorMatchSize: the minimum number of orbs in a color match
- 	minWordMatchSize: the minimum number of orbs in a word match 
- 	colors: color initials of orbs to be generated. out of RGBLDW
- 	movement: swap or time
-	moveLimit: single, multi, or hybrid. can the player make more than one drag per turn?
-	           hybrid allows this but incurs a greater cost for the first move of a drag.
- 	maxSwaps: 20
- 	maxTime: 5 (seconds)
-*/
-
 var config = {
 	totalRows : 6,
 	totalCols : 7,
